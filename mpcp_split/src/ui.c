19bcda7d2c2d086714a8bc9c53316a9b4c8c2b9f
PowerTea <vitoprdito8@gmail.com>
2026-03-23 21:53:02 +0100

    Delete mpcp_split directory

--- mpcp_split/src/ui.c
+++
@@ -1,6 +0,0 @@
-/* AethroSync — src/ui.c — colour UI state */
-#define _GNU_SOURCE
-#include "../include/mpcp.h"
-
-/* Single definition of g_ui_colour (all TUs get extern from mpcp.h) */
-bool g_ui_colour = false;
