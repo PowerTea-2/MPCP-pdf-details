3501c04a2e65ceea2e4ac86d8776fabfe26eb817
PowerTea <vitoprdito8@gmail.com>
2026-04-12 03:15:28 +0200

    Delete AethroSync_v1_final directory

--- AethroSync_v1_final/src/ui.c
+++
@@ -1,6 +0,0 @@
-/* AethroSync — src/ui.c — colour UI state */
-#include "../include/platform.h"
-#include "../include/mpcp.h"
-
-/* Single definition of g_ui_colour (all TUs get extern from mpcp.h) */
-bool g_ui_colour = false;
